c41202fd0d3b1a5374b6b00a43a84218a088e326
Alamin <mdalaminhossain573190@gmail.com>
2024-04-22 20:01:56 +0600

    added update part

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
+    app.get('/users/:id',async(req,res) =>{
+      const id =req.params.id;
+      const query ={_id:new ObjectId(id)}
+      const user =await usersCollection.findOne(query);
+      res.send(user);
+    })
 
     app.post('/users',async(req,res)=>{
         const user =req.body;
@@ -43,6 +49,24 @@ async function run() {
         const result = await usersCollection.insertOne(user);
         res.send(result);
     })
+
+    app.put('/users/:id',async(req, res) =>{
+      const id =req.params.id;
+      const user =req.body;
+      console.log(id,user)
+      const filter ={_id: new ObjectId(id)}
+      const options ={upsert:true}
+      const updateUser ={
+        $set:{
+          name:user.name ,
+          email:user.email
+        }
+      }
+      const result = await usersCollection.updateOne(filter,updateUser,options)
+      res.send(result);
+    })
+
+
     app.delete('/users/:id',async(req ,res) =>{
         const id = req.params.id;
         console.log('please delete from database',id)
